3aeccda9c3f2777320c190e5cdc614591393855e
N.Adithya Sharma <165984046+Adithya2608@users.noreply.github.com>
2026-08-03 19:57:23 +0530

    Add apb_test class for UVM testing

---
+++ apb_test.sv
@@ -0,0 +1,41 @@
+`ifndef APB_TEST_SV
+`define APB_TEST_SV
+
+class apb_test extends uvm_test;
+
+    `uvm_component_utils(apb_test)
+
+    apb_env env;
+    apb_sequence seq;
+
+    // Constructor
+    function new(string name="apb_test",
+                 uvm_component parent);
+        super.new(name,parent);
+    endfunction
+
+    // Build Phase
+    function void build_phase(uvm_phase phase);
+
+        super.build_phase(phase);
+
+        env = apb_env::type_id::create("env", this);
+
+    endfunction
+
+    // Run Phase
+    task run_phase(uvm_phase phase);
+
+        phase.raise_objection(this);
+
+        seq = apb_sequence::type_id::create("seq");
+
+        seq.start(env.agent.seqr);
+
+        phase.drop_objection(this);
+
+    endtask
+
+endclass
+
+`endif
